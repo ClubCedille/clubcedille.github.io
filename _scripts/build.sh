e98a7dfbb9e397a95226afc965985b63b697ae19
Club Cedille - Webmaster <cedille@ens.etsmtl.ca>
2015-09-29 21:35:33 -0400

    Try to change de script to build

--- _scripts/build.sh
+++ _scripts/build.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-if [[ $TRAVIS_BRANCH == 'master' ]] ; then
-  bundle exec rake stage
-  bundle install
-  bundle exec jekyll build
-  bundle exec htmlproof ./_site
-  jekyll build
-else
-  echo 'Invalid branch. You can only deploy from master.'
-  exit 1
-fi
+bundle exec rake stage
+bundle install
+bundle exec jekyll build
+bundle exec htmlproof ./_site
+jekyll build
