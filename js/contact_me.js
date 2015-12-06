7957df0914fa9ed166637301da66c9678f4709d5
Dmitri Kolytchev <kolytchev.d@gmail.com>
2015-12-06 00:32:32 -0500

    Finalizing forms and requests

--- js/contact_me.js
+++ js/contact_me.js
@@ -2,11 +2,28 @@
 ---
 $(function() {
 
-   $('#contact .form-selector').on('click', 'a', function(){
-       $('#contact .google-form').addClass('hidden');
-       $('#contact').find($(this).data('target')).removeClass('hidden');
-       
-       $('#contact .form-selector a').removeClass('active');
-       $(this).addClass('active');
-   }) 
+	$('#contact .form-selector').on('click', 'a', function(){
+		var t = $(this).data('target');
+		$('#contact-form, #adhesion-form').not(t).addClass('hidden');
+		$(t).removeClass('hidden');
+
+		$('#contact .form-selector .active').removeClass('active');
+		$(this).addClass('active');
+	})
+
+	$('body').on('submit', '#contact-form,#adhesion-form', function(e){
+		e.preventDefault();
+		var data = {};
+		
+		$(this).serializeArray().map(function(x){
+			// warning: doesn't handle multiple checked or selected options.
+			// source: http://stackoverflow.com/questions/2591354/jquery-form-serialize-empty-string
+			data[x.name] = x.value;
+		});
+
+		$.post($(this).attr('action'), data, function response(d){
+			console.log(d);
+		}, 'text/json');
+
+	});
 });
